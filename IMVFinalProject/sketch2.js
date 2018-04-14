a4d30a6d00d49974f3a9bd5240e1b59c908787fe
Daniel Mullins <danmullins104@gmail.com>
2018-04-14 10:44:27 -0400

    yt

---
+++ IMVFinalProject/sketch2.js
@@ -0,0 +1,52 @@
+var song;
+var amp;
+var yoff = 0.0
+
+function setup() {
+  createCanvas(800,500);
+  song = loadSound("rockstar.mp3", loaded);
+  amp = new p5.Amplitude();
+
+}
+
+function loaded (){
+  song.play();
+  draw();
+}
+
+function draw() {
+ background(0);
+
+stroke(255, 20, 90);
+  noFill();
+
+  beginShape();
+    
+  var xoff= 0;
+    
+  for (var x = 0; x <= width; x += 10) {
+  	 var level = amp.getLevel();
+ console.log(level);
+ var size = map(level, 0, 1, 0, 200);
+
+  // Map noise value (between 0 and 1) to y-value of canvas
+    var y = map(level, noise(xoff, yoff), 0, 1, 400, 500);
+    // Set the vertex
+    vertex(x, y); 
+    xoff += 0.05;
+  }
+    
+  //Speed of moving waves
+  yoff += 0.008;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
+
+}
+
+function mousePressed() {
+  setup();
+
+}
+
+
