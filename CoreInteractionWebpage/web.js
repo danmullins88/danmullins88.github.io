4a3686bb65b6bdc1fcdf0f1e3810074b10fae58f
Daniel Mullins <danmullins104@gmail.com>
2018-05-09 11:30:48 -0400

    webpage update :apple:

---
+++ CoreInteractionWebpage/web.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(windowWidth, 2100);
+  background(0, 0, 0);
+  smooth();
+
+  var xstart = random(10),
+      xnoise = xstart,
+      ynoise = random(10);
+
+  for (var y = 0; y <= height; y+=5) {
+    ynoise += 0.1;
+    xnoise  = xstart;
+    for (var x = 0; x <= width; x+=5) {
+      xnoise += 0.1;
+      drawPoint(x, y, noise(xnoise, ynoise));
+    }
+  }
+}
+
+function drawPoint(x, y, noiseFactor) {
+  push();
+  translate(x, y);
+  rotate(noiseFactor * radians(300));
+
+  stroke(204,177,68);
+  line(0, 0, 7, 2);
+  pop();
+}
